ef347cd1524ea6c55232f81ca7b24ce3cecb6cae
Sachin Gorade <sachin.gorade@gmail.com>
2019-12-11 16:05:41 +0100

    Initial commit

---
+++ src/test/java/com/sach/learning/jenkins/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sach.learning.jenkins.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
